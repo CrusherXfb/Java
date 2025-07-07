a52e44115243419a26f640b97d59a7465949d13c
Crusher <CrusherXfb@outlook.com>
2025-07-07 14:57:43 +0300

    PrimeNumbers

--- src/Stream/PrimeNumbersInArray.java
+++ src/Stream/PrimeNumbersInArray.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-public class PrimeNumbers {
+public class PrimeNumbersInArray {
 
     public static void main(String[] args) {
-        int n = 50; // Пример значения n
+        int n = 50;
         int[] array = new int[n];
 
-        // Заполнение массива случайными числами от 0 до 100
         Random random = new Random();
         for (int i = 0; i < n; i++) {
             array[i] = random.nextInt(101);
@@ -18,18 +17,16 @@ public class PrimeNumbers {
 
         System.out.println("Исходный массив: " + Arrays.toString(array));
 
-        // Параллельная фильтрация и сортировка простых чисел
         int[] primes = IntStream.range(0, n)
                 .parallel()
                 .map(i -> array[i])
-                .filter(PrimeNumbers::isPrime)
+                .filter(PrimeNumbersInArray::isPrime)
                 .sorted()
                 .toArray();
 
         System.out.println("Простые числа в диапазоне: " + Arrays.toString(primes));
     }
 
-    // Метод для проверки, является ли число простым
     public static boolean isPrime(int num) {
         if (num <= 1) {
             return false;
